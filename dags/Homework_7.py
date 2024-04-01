b7d1bc303c83bd4dff7d1ea920eb9156191a850d
KonstantinNaftaliev <119065434+KonstantinNaftaliev@users.noreply.github.com>
2024-04-01 20:29:38 +0300

    Update Homework_7.py

--- dags/Homework_7.py
+++ dags/Homework_7.py
@@ -22,7 +22,7 @@ def choosing_description_weather(ti = None):
         return 'warm_branch'
     return 'cold_branch'
 
-dz7dag = DAG( 'dz7dag', description= 'DZ& dag',
+Homework_7 = DAG( 'Homework_7_Naftaliev', description= 'Homework_7',
     schedule_interval= '0 12 * * *' ,
     start_date= datetime(2024, 3, 29),
     catchup= False 
@@ -31,16 +31,16 @@ dz7dag = DAG( 'dz7dag', description= 'DZ& dag',
 get_weather_task = PythonOperator(
     task_id='get_weather_data',
     python_callable=get_weather_data,
-    dag=dz7dag
+    dag=Homework_7
 )
 
 print_task = BranchPythonOperator(
     task_id='print_task',
     python_callable=choosing_description_weather,
-    dag=dz7dag
+    dag=Homework_7
 )
 
-bash_print_warm = BashOperator(task_id = 'warm_branch', bash_command='echo "WARM"', dag = dz7dag)
-bash_print_cold = BashOperator(task_id = 'cold_branch', bash_command='echo "COLD"', dag = dz7dag)
+bash_print_warm = BashOperator(task_id = 'warm_branch', bash_command='echo "WARM"', dag = Homework_7)
+bash_print_cold = BashOperator(task_id = 'cold_branch', bash_command='echo "COLD"', dag = Homework_7)
 
 get_weather_task >> print_task >> [bash_print_warm, bash_print_cold]
